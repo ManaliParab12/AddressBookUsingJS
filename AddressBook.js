16f29dde99fd3ba3ee47fd2c553680e493e5beab
ManaliParab12 <manaliparab10@gmail.com>
2021-01-01 17:59:09 +0530

    Created function to search person in perticular city or state

--- AddressBook.js
+++ AddressBook.js
@@ -203,3 +203,20 @@ function duplicateEntry() {
 });
 }
 duplicateEntry();
+
+//UC8 
+
+function searchbyCityOrState() {
+    let inputCity = prompt("Enter city to search person : ");
+    let inputState = prompt("Enter state to search person : ");
+    addressBookArray.forEach(addressBook => {
+        if(addressBook.city == inputCity || addressBook.state == inputState) {
+            console.log("Person in city " +inputCity+ " and state " +inputState);
+        }
+    });
+    addressBookArray.filter(contact => contact.city == inputCity)
+    addressBookArray.filter(contact => contact.city == inputCity)
+    addressBookArray.forEach(contact => console.log(addressBookArray));
+}
+
+searchbyCityOrState();
