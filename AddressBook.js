9f96408e31033987228eae63d6c0063a5f91b1c2
ManaliParab12 <manaliparab10@gmail.com>
2021-01-01 17:43:58 +0530

    Found existing contact and edited

--- AddressBook.js
+++ AddressBook.js
@@ -127,13 +127,44 @@ addressBookArray.push(addressBook2);
 console.log("\nNew Contact Added to Array " +addressBookArray);
 
 //UC4
-let addressBookMap = new Map();
-addressBookMap.set(addressBook.firstName, addressBook);
-addressBookMap.set(addressBook1.firstName, addressBook1);
-addressBookMap.set(addressBook2.firstName, addressBook2);
+const prompt = require('prompt-sync')({sigint: true});
 
-let findName = addressBookMap.get("Manali");
-findName.lastName = "Tambade";
-console.log(addressBookMap.get("Manali"));
+function editPerson() {
+let userInput = prompt("Enter name to update Contact : ");
 
+addressBookArray.forEach(addressBook => {
+    if(addressBook.firstName == userInput) {
+    console.log("1.PhoneNumber" + "\n2.Address" + "\n3.quit");
+    var choice = prompt("Select any option to edit :")
+
+    switch(choice) {
+        case "1" :
+            phoneNumber = prompt("Enter new Phone Number :");
+            addressBook.phoneNumber = phoneNumber;
+            console.log(addressBook.toString());
+            console.log("Phone number updated")
+        
+            break;
+        case "2" :
+            city = prompt("Enter new city : ");
+            addressBook.city = city;
+            console.log(addressBook.toString());
+
+            state = prompt("Enter new state : ");
+            addressBook.state = state;
+            console.log(addressBook.toString());
+
+            zip = prompt("Enter new zip : ");
+            addressBook.zip = zip;
+            console.log(addressBook.toString());
+            break;
+        default :
+            console.log("Incorrect choice");
+            break;
+    }  
+}
+}); 
+}
+
+editPerson();
 
