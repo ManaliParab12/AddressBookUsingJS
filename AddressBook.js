9751256db88c79b70ba4b49b5adb8b75111c4e38
ManaliParab12 <manaliparab10@gmail.com>
2021-01-01 17:56:17 +0530

    Created function to check duplicate entry in book

--- AddressBook.js
+++ AddressBook.js
@@ -190,3 +190,16 @@ function numberOfContact() {
 }
 
 numberOfContact();
+
+// UC7 
+function duplicateEntry() {
+    let userInput = prompt("Enter name to check Entry in addressbook : ");
+    addressBookArray.forEach(addressBook => {
+        if(addressBook.firstName == userInput) {
+            console.log("Contact already exists in the book");
+        } else {
+            console.log("Contact not exists in the book");
+        }
+});
+}
+duplicateEntry();
