edde068189b41acbdeced2e6b01f1cc2f8590cc6
ManaliParab12 <manaliparab10@gmail.com>
2021-01-01 18:04:49 +0530

    Created function to find number of contact in address book by city or state

--- AddressBook.js
+++ AddressBook.js
@@ -234,3 +234,14 @@ function viewByCityOrState() {
 }
 
 viewByCityOrState();
+
+//UC10
+function countByCity(city) {
+    return addressBookArray.filter(contact => contact.city == city).reduce((count, contact) => count += 1, 0);
+}
+function countByState(state) {
+    return addressBookArray.filter(contact => contact.state == state).reduce((count, contact) => count += 1, 0)
+}
+console.log("number of Person in city " +countByCity("Virar"));
+console.log("number of Person in state " +countByState("Maharashtra"));
+ 
