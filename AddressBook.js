64c1d90bdf6580d034df64dd583b85dd704981c4
ManaliParab12 <manaliparab10@gmail.com>
2021-01-01 17:52:42 +0530

    Created function to find number of contact in address book

--- AddressBook.js
+++ AddressBook.js
@@ -182,3 +182,11 @@ function deleteContact() {
 }
 
 deleteContact();
+
+//UC6
+function numberOfContact() {
+    let totalCount =  addressBookArray.reduce(((totalCount) => { totalCount +=1; return totalCount; }), 0);
+    console.log("Total Number of Contacts in AddressBook : " +totalCount);
+}
+
+numberOfContact();
